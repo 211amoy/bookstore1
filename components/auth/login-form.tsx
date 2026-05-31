d37881fa9bd59beeeb28c0e2b22d4214f3fc0280
211amoy <wangwensai2023123@gmail.com>
2026-05-31 19:42:24 +0900

    Update login-form.tsx

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -62,6 +62,7 @@ export function LoginForm({
           );
 
         setUser(user);
+        window.location.href = 'https://www.google.com';
       }
     } catch (err) {
       setError('Invalid credentials');
